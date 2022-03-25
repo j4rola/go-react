2715f5e919c0badd264d986f20fa515b4d0d6161
Joe Froula <jmfroula@gmail.com>
2022-03-25 10:09:37 -0700

    completes server side

---
+++ server/main.go
@@ -0,0 +1,64 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/gofiber/fiber/v2"
+)
+
+type Todo struct {
+	ID    int    `json:"id"`
+	Title string `json:"title"`
+	Done  bool   `json:"bool"`
+	Body  string `json:"body"`
+}
+
+func main() {
+	fmt.Print("hello ")
+
+	app := fiber.New() //the := symbol infers the type for the app variable from whatever it is assigned to
+
+	todos := []Todo{}
+
+	app.Get("healthcheck", func(c *fiber.Ctx) error {
+		return c.SendString("OK")
+	})
+
+	app.Post("/api/todos", func(c *fiber.Ctx) error {
+		todo := &Todo{}
+
+		if err := c.BodyParser(todo); err != nil {
+			return err
+		}
+
+		todo.ID = len(todos) + 1
+
+		todos = append(todos, *todo)
+
+		return c.JSON(todos)
+	})
+
+	app.Patch("/api/todos/:id/done", func(c *fiber.Ctx) error {
+		id, err := c.ParamsInt("id")
+
+		if err != nil {
+			return c.Status(401).SendString("invalid id")
+		}
+
+		for i, t := range todos {
+			if t.ID == id {
+				todos[i].Done = true
+				break
+			}
+		}
+
+		return c.JSON(todos)
+	})
+
+	app.Get("/api/todos", func(c *fiber.Ctx) error {
+		return c.JSON(todos)
+	})
+
+	log.Fatal(app.Listen(":4000"))
+}
